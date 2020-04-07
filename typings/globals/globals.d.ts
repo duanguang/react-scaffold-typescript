73090d442510722d2fd048cd8a2bf19fa6aaf7eb
duanguang <492466509@qq.com>
2020-04-07 17:11:33 +0800

    first commit

---
+++ typings/globals/globals.d.ts
@@ -0,0 +1,11 @@
+/*import ComponentClass = __React.ComponentClass;*/
+
+declare var __DEV__:boolean;
+
+interface window extends Window {
+    __initialState__:any;
+    devToolsExtension:Function;
+    opera:any;
+}
+
+
---
+++ typings/index.d.ts
@@ -0,0 +1,3 @@
+
+
+/// <reference path="globals/globals.d.ts" />
