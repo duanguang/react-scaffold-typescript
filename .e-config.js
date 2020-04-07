d29ac22e8a13c19053588ddef3bfd94d2889ebfd
duanguang <492466509@qq.com>
2020-04-07 17:19:55 +0800

    first commit

--- .e-config.js
+++ .e-config.js
@@ -158,58 +158,5 @@ module.exports = function (configs) {
     });
     return configs;
 };
-/*module.exports = {
-    "name":"default",
-    "apps":['home'],
-    "open": false,
-    "defaultPort": 8002,
-    "server": "127.0.0.1",
-    "imageInLineSize": 8192,
-    "publicPath": "/assets/",
-    "devServer": {
-        "noInfo": true,
-        "proxy": undefined
-         //proxy: [{
-         //context: ['/**', '!/static/**', '!/webpack/**', '!/webpack-dev-server/**', '!/sockjs-node/**', '!/index.html'],
-         //target: 'http://tstmanage.360kad.com
-         //',
-         //changeOrigin: true
-         //}]
-    },
-    "postcss": {
-        "autoprefixer": {
-            "browsers": [
-                "last 2 versions",
-                "Firefox ESR",
-                "> 1%",
-                "ie >= 8"
-            ]
-        }
-    },
-    "webpack": {
-        "dllConfig": {
-            "vendors": ['react']
-        }
-    },
-    "babel": {
-        "query": {
-            "presets": [
-                "es2015",
-                "stage-0",
-                "react"
-            ],
-            "cacheDirectory": true,
-             "plugins": [
-            "add-module-exports",
-            "transform-runtime",
-            "transform-decorators-legacy",
-        ]
-        }
-    },
-    "htmlWebpackPlugin": {
-        "title": "",
-        "appMountId": "",
-    },
-    "entries": ['src/index']
-};*/
+
 
