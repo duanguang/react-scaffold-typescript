f2a116921682abdf06246831b4b06f33a73ec907
duanguang <duanguang@hoolinks.com>
2020-08-26 13:54:04 +0800

    fix: 修复webpack4.x 运行mobile web react16.x 依赖包版本获取镜像路径

--- src/home/containers/App.tsx
+++ src/home/containers/App.tsx
@@ -1,20 +1,20 @@
-import { Route, Switch } from 'legions/router';
+import { Route,Switch } from 'legions/router';
 import React from 'react';
 import '../assets/css/theme.less';
 
 
 export default class App extends React.Component {
-    constructor(){
-        super()
+    constructor(props: {}) {
+        super(props)
     }
     render() {
         return (
-                <Switch>
+            <Switch>
 
-                    {/* <Route path="/admin" component={HomeManage}></Route> */}
-                    {/* <Route path="/" component={LcmTest}></Route> */}
-                    {/* <Route component={Todo}/> */}
-                </Switch>
+                {/* <Route path="/admin" component={HomeManage}></Route> */}
+                {/* <Route path="/" component={LcmTest}></Route> */}
+                {/* <Route component={Todo}/> */}
+            </Switch>
         );
     }
 }
--- src/home/index.ts
+++ src/home/index.ts
@@ -2,15 +2,15 @@ import '@babel/polyfill';
 import create from 'legions';
 import App from './containers/App';
 /* import '../common/components/nprogress/index.css'; */
-const app = create({enableDevTools:false,router: true,history:null});
-app.start(App, '#react-lcm');
+const app = create({ enableDevTools: false, router: true, history: null });
+app.start(App, '#app');
 
 /* if (!global['_babelPolyfill']) {
     // 为了解决重复引入的问题
     require('babel-polyfill');
 } */
 // @ts-ignore
-if (module.hot&&process.env.NODE_ENV==='dev') {
-    // @ts-ignore
-    module.hot.accept();
+if (module.hot && process.env.NODE_ENV === 'dev') {
+  // @ts-ignore
+  module.hot.accept();
 }
