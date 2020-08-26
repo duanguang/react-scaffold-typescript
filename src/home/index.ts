12ff069bd80bac78a2662a408b5989d4aca88939
duanguang <duanguang@hoolinks.com>
2020-08-26 10:52:23 +0800

    fix: 修复webpack4.x 运行pc web react15.x 业务组件库报错

--- src/home/index.ts
+++ src/home/index.ts
@@ -1,15 +1,15 @@
-import  create from 'legions';
+import create from 'legions';
 import App from './containers/App';
-import '../common/components/nprogress/index.css';
-const app = create({enableDevTools:false,router: true,history:null});
-app.start(App, '#react-lcm');
+/* import '../common/components/nprogress/index.css'; */
+const app = create({ enableDevTools: false, router: true, history: null });
+app.start(App, '#app');
 
 if (!global['_babelPolyfill']) {
-    // 为了解决重复引入的问题
-    require('babel-polyfill');
+  // 为了解决重复引入的问题
+  require('babel-polyfill');
 }
 // @ts-ignore
-if (module.hot&&process.env.NODE_ENV==='dev') {
-    // @ts-ignore
-    module.hot.accept();
+if (module.hot && process.env.NODE_ENV === 'dev') {
+  // @ts-ignore
+  module.hot.accept();
 }
