cdcef45c58a3aaba3e9048a8d6634694a1474be7
duanguang <duanguang@hoolinks.com>
2020-11-09 17:27:56 +0800

    chore(webpack4.x-rect16.x): 更新依赖版本

--- src/home/stores/TodoStore.ts
+++ src/home/stores/TodoStore.ts
@@ -1,44 +1,44 @@
 import StoreBase, { IStoreBaseMeta } from '../../common/stores/StoreBase';
-import {observable,autorun,action, computed} from 'mobx';
-import { observablePromise,ObservableTempState } from 'legions/store-utils';
+import { observable, autorun, action, computed } from 'mobx';
+import { observablePromise, ObservableTempState } from 'legions/store-utils';
 import { MockContainerEntity } from '../models/mockEntity';
 import { getMockData } from '../services/message';
-interface IContext{
-   /*  UserInfoApp:UserInfoStore, */
+interface IContext {
+  /*  UserInfoApp:UserInfoStore, */
 }
-export interface ITriggerEventPrams{
-    payload:{
-        payloadModel:string;
-        b:number;
-    }
+export interface ITriggerEventPrams {
+  payload: {
+    payloadModel: string;
+    b: number;
+  };
 }
-export default class TodoStore extends StoreBase<IContext>{
-    static meta :IStoreBaseMeta={
-        ...StoreBase.meta,
-        className:'TodoStore',
-        /* eventScopes: [User], */
-        contextTypes:{
-            /* UserInfoApp:UserInfoStore, */
-        },
-    }
+export default class TodoStore extends StoreBase<IContext> {
+  static meta: IStoreBaseMeta = {
+    ...StoreBase.meta,
+    className: 'TodoStore',
+    /* eventScopes: [User], */
+    contextTypes: {
+      /* UserInfoApp:UserInfoStore, */
+    },
+  };
 
-    @observable obMockData=observablePromise<MockContainerEntity>();
+  @observable obMockData = observablePromise<MockContainerEntity>();
 
-    constructor(context){
-        super(context);
-        this.watch()
-    }
+  constructor(context) {
+    super(context);
+    this.watch();
+  }
 
-    watch=()=>{
-        autorun(()=> {
-            console.log('数据状态',this.obMockData.state)
-         });
-    }
+  watch = () => {
+    autorun(() => {
+      console.log('数据状态', this.obMockData.state);
+    });
+  };
 
-    @action getMockData(){
-        this.obMockData=observablePromise(getMockData());
-    }
-    triggerEvent(payload:ITriggerEventPrams) {
-        /* this.context.dispatch(User.created,payload); */
-    }
+  @action getMockData() {
+    this.obMockData = observablePromise(getMockData());
+  }
+  triggerEvent(payload: ITriggerEventPrams) {
+    /* this.context.dispatch(User.created,payload); */
+  }
 }
